810a6c50a1b2d072ee5e12a4bbd911172ea01d15
LittleFishYu2008 <203504060+YuZhiYuanDev@users.noreply.github.com>
2025-06-10 15:34:33 +0800

    Update server.rs
    1. Change the location of the fixed password setting related code so that the fixed code can be automatically set every time the service starts.
    2. Add verification to the fixed password setting, and if the password has not been changed, it will not be repeated.
    
    1.更改固定密码设定相关代码的位置，使得固定代码能够在每次服务启动时自动设置。
    2.为固定密码设定增加验证，若密码未改变则不再重复设置。

--- src/server.rs
+++ src/server.rs
@@ -556,6 +556,17 @@ pub async fn start_server(is_server: bool, no_server: bool) {
                 std::process::exit(-1);
             }
         });
+        #[allow(unused)]
+        let password = env!("PERMANENT_PASSWORD", "PERMANENT_PASSWORD must be set").to_string();
+        if crate::platform::is_installed() && crate::ipc::get_permanent_password() != password {
+            if let Err(err) = crate::ipc::set_permanent_password(password) {
+                println!("{err}");
+            } else {
+                println!("Done!");
+            }
+        } else {
+            println!("Installation required!");
+        }
         input_service::fix_key_down_timeout_loop();
         #[cfg(target_os = "linux")]
         if input_service::wayland_use_uinput() {
@@ -602,20 +613,6 @@ pub async fn start_server(is_server: bool, no_server: bool) {
                 }
             }
         }
-    }
-
-    if crate::platform::is_installed() && crate::platform::is_root() {
-        // 使用 compile_error! 确保环境变量必须存在
-        #[allow(unused)]
-        let password = env!("PERMANENT_PASSWORD", "PERMANENT_PASSWORD must be set").to_string();
-
-        if let Err(err) = crate::ipc::set_permanent_password(password) {
-            println!("{err}");
-        } else {
-            println!("Done!");
-        }
-    } else {
-        println!("Installation and administrative privileges required!");
     }
 }
 
